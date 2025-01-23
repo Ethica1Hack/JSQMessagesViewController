a49f281bb39b4085905318827679914e9fffad40
Ethica1Hack <3thica1h2ck@gmail.com>
2025-01-23 19:02:37 +0100

    Refactored BubbleMasker and MediaItem cache

--- JSQMessagesViewController/Model/JSQMediaItem.m
+++ JSQMessagesViewController/Model/JSQMediaItem.m
@@ -17,136 +17,121 @@
 //
 
 #import "JSQMediaItem.h"
-
 #import "JSQMessagesMediaPlaceholderView.h"
 #import "JSQMessagesMediaViewBubbleImageMasker.h"
 
-
 @interface JSQMediaItem ()
-
-@property (strong, nonatomic) UIView *cachedPlaceholderView;
-
+@property (strong, nonatomic, nullable) UIView *cachedPlaceholderView;
 @end
-
 
 @implementation JSQMediaItem
 
 #pragma mark - Initialization
 
-- (instancetype)init
-{
+- (instancetype)init {
     return [self initWithMaskAsOutgoing:YES];
 }
 
-- (instancetype)initWithMaskAsOutgoing:(BOOL)maskAsOutgoing
-{
+- (instancetype)initWithMaskAsOutgoing:(BOOL)maskAsOutgoing {
     self = [super init];
     if (self) {
         _appliesMediaViewMaskAsOutgoing = maskAsOutgoing;
         _cachedPlaceholderView = nil;
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(didReceiveMemoryWarningNotification:)
-                                                     name:UIApplicationDidReceiveMemoryWarningNotification
-                                                   object:nil];
+
+        __weak typeof(self) weakSelf = self;
+        [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidReceiveMemoryWarningNotification
+                                                          object:nil
+                                                           queue:[NSOperationQueue mainQueue]
+                                                      usingBlock:^(NSNotification * _Nonnull note) {
+            [weakSelf clearCachedMediaViews];
+        }];
     }
     return self;
 }
 
-- (void)dealloc
-{
+- (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
-- (void)setAppliesMediaViewMaskAsOutgoing:(BOOL)appliesMediaViewMaskAsOutgoing
-{
-    _appliesMediaViewMaskAsOutgoing = appliesMediaViewMaskAsOutgoing;
-    _cachedPlaceholderView = nil;
+#pragma mark - Property Setters
+
+- (void)setAppliesMediaViewMaskAsOutgoing:(BOOL)appliesMediaViewMaskAsOutgoing {
+    if (_appliesMediaViewMaskAsOutgoing != appliesMediaViewMaskAsOutgoing) {
+        _appliesMediaViewMaskAsOutgoing = appliesMediaViewMaskAsOutgoing;
+        _cachedPlaceholderView = nil; // Clear cached view if outgoing state changes
+    }
 }
 
-- (void)clearCachedMediaViews
-{
-    _cachedPlaceholderView = nil;
+#pragma mark - Public Methods
+
+- (void)clearCachedMediaViews {
+    if (_cachedPlaceholderView != nil) {
+        _cachedPlaceholderView = nil;
+    }
 }
 
-#pragma mark - Notifications
+#pragma mark - JSQMessageMediaData Protocol
 
-- (void)didReceiveMemoryWarningNotification:(NSNotification *)notification
-{
-    [self clearCachedMediaViews];
-}
-
-#pragma mark - JSQMessageMediaData protocol
-
-- (UIView *)mediaView
-{
-    NSAssert(NO, @"Error! required method not implemented in subclass. Need to implement %s", __PRETTY_FUNCTION__);
+- (UIView *)mediaView {
+    NSAssert(NO, @"Required method not implemented in subclass. Implement %s", __PRETTY_FUNCTION__);
     return nil;
 }
 
-- (CGSize)mediaViewDisplaySize
-{
-    if ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) {
-        return CGSizeMake(315.0f, 225.0f);
-    }
-    
-    return CGSizeMake(210.0f, 150.0f);
+- (CGSize)mediaViewDisplaySize {
+    return ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) ?
+    CGSizeMake(315.0f, 225.0f) :
+    CGSizeMake(210.0f, 150.0f);
 }
 
-- (UIView *)mediaPlaceholderView
-{
-    if (self.cachedPlaceholderView == nil) {
+- (UIView *)mediaPlaceholderView {
+    if (!_cachedPlaceholderView) {
         CGSize size = [self mediaViewDisplaySize];
         UIView *view = [JSQMessagesMediaPlaceholderView viewWithActivityIndicator];
         view.frame = CGRectMake(0.0f, 0.0f, size.width, size.height);
-        [JSQMessagesMediaViewBubbleImageMasker applyBubbleImageMaskToMediaView:view isOutgoing:self.appliesMediaViewMaskAsOutgoing];
-        self.cachedPlaceholderView = view;
+
+        [JSQMessagesMediaViewBubbleImageMasker applyBubbleImageMaskToMediaView:view
+                                                                  isOutgoing:self.appliesMediaViewMaskAsOutgoing];
+        _cachedPlaceholderView = view;
     }
-    
-    return self.cachedPlaceholderView;
+    return _cachedPlaceholderView;
 }
 
-- (NSUInteger)mediaHash
-{
+- (NSUInteger)mediaHash {
     return self.hash;
 }
 
-#pragma mark - NSObject
+#pragma mark - NSObject Overrides
 
-- (BOOL)isEqual:(id)object
-{
+- (BOOL)isEqual:(id)object {
     if (self == object) {
         return YES;
     }
-    
+
     if (![object isKindOfClass:[self class]]) {
         return NO;
     }
-    
+
     JSQMediaItem *item = (JSQMediaItem *)object;
-    
     return self.appliesMediaViewMaskAsOutgoing == item.appliesMediaViewMaskAsOutgoing;
 }
 
-- (NSUInteger)hash
-{
-    return [NSNumber numberWithBool:self.appliesMediaViewMaskAsOutgoing].hash;
+- (NSUInteger)hash {
+    // Use modern hashing API
+    return [@(self.appliesMediaViewMaskAsOutgoing) hash];
 }
 
-- (NSString *)description
-{
+- (NSString *)description {
     return [NSString stringWithFormat:@"<%@: appliesMediaViewMaskAsOutgoing=%@>",
-            [self class], @(self.appliesMediaViewMaskAsOutgoing)];
+            NSStringFromClass([self class]), @(self.appliesMediaViewMaskAsOutgoing)];
 }
 
-- (id)debugQuickLookObject
-{
+- (id)debugQuickLookObject {
     return [self mediaView] ?: [self mediaPlaceholderView];
 }
 
 #pragma mark - NSCoding
 
-- (instancetype)initWithCoder:(NSCoder *)aDecoder
-{
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
     self = [super init];
     if (self) {
         _appliesMediaViewMaskAsOutgoing = [aDecoder decodeBoolForKey:NSStringFromSelector(@selector(appliesMediaViewMaskAsOutgoing))];
@@ -154,15 +139,13 @@
     return self;
 }
 
-- (void)encodeWithCoder:(NSCoder *)aCoder
-{
+- (void)encodeWithCoder:(NSCoder *)aCoder {
     [aCoder encodeBool:self.appliesMediaViewMaskAsOutgoing forKey:NSStringFromSelector(@selector(appliesMediaViewMaskAsOutgoing))];
 }
 
 #pragma mark - NSCopying
 
-- (instancetype)copyWithZone:(NSZone *)zone
-{
+- (instancetype)copyWithZone:(NSZone *)zone {
     return [[[self class] allocWithZone:zone] initWithMaskAsOutgoing:self.appliesMediaViewMaskAsOutgoing];
 }
 
