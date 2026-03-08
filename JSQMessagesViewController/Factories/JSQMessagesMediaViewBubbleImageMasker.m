09098c988352b49197b609c7f7a97ded7fb9e3b1
Ethica1Hack <3thica1h2ck@gmail.com>
2026-03-08 16:43:58 +0100

    Added header for solidBubbleImage

--- JSQMessagesViewController/Factories/JSQMessagesMediaViewBubbleImageMasker.m
+++ JSQMessagesViewController/Factories/JSQMessagesMediaViewBubbleImageMasker.m
@@ -18,6 +18,7 @@
 
 #import "JSQMessagesMediaViewBubbleImageMasker.h"
 #import "JSQMessagesBubbleImageFactory.h"
+#import "UIImage+JSQMessages.h"
 
 @implementation JSQMessagesMediaViewBubbleImageMasker
 
